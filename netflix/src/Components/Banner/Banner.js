356272aade99cc0d65342137ff8dcbd5be178832
athulkrishna <athulkrishna4232@gmail.com>
2022-06-13 23:52:58 +0530

    banner image added dynamically

--- netflix/src/Components/Banner/Banner.js
+++ netflix/src/Components/Banner/Banner.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Banner.css"
+import axios from '../../axios'
+import {APIKey , imageUrl} from '../../Constats/constats'
 
 function Banner() {
+  const [movie, setMovie] = useState()
+  useEffect(() => {
+
+    axios.get(`trending/all/week?api_key=${APIKey}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0])
+      setMovie(response.data.results[0])
+    })
+  
+  
+  }, [])
+  
   return (
-    <div className='banner'>
+    <div
+    style={{backgroundImage : `url(${movie ?  imageUrl+ movie.backdrop_path : " "})`}} 
+     className='banner'>
         <div className='content'>
-            <h1  className='title'>Movie Name</h1>
+            <h1  className='title'>{movie ? movie.name :""}</h1>
             <div className='banner_button'>
                 <button className='button'>Play</button>
                 <button className='button'>My list</button>
             </div>
-            <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without</h1>
+            <h1 className='description'>{movie ? movie.overview : ""}t</h1>
 
         </div>
 
