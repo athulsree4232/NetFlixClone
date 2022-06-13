0776699b7ef923a34252e6083b3d51d1e6fe5e20
athulkrishna <athulkrishna4232@gmail.com>
2022-06-14 00:46:11 +0530

    movie list created dynamically

---
+++ netflix/src/urls.js
@@ -0,0 +1,4 @@
+import {APIKey} from './Constats/constats'
+
+export const orginals = `discover/tv?api_key=${APIKey}&with_networks=213`
+export const action = `discover/movie?api_key=${APIKey}&with_genres=28`
