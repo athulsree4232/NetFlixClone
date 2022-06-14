29ceac173652f9ec8a7500f799894d696fd0018d
athulkrishna <athulkrishna4232@gmail.com>
2022-06-14 13:17:57 +0530

    Different categories of movies added

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
-import {orginals , action} from './urls'
+import {orginals , action, comedy, horror, romance, documentries} from './urls'
 import "./App.css"
 import Banner from "./Components/Banner/Banner";
 import RowPost from "./Components/RowPost/RowPost";
@@ -12,6 +12,10 @@ function App() {
       <Banner/>
       <RowPost title = "Netflix Orginals" url={orginals}/>
       <RowPost title = "Action" isSmall url={action}/>
+      <RowPost title = "Comedy" isSmall url={comedy}/>
+      <RowPost title = "Horror" isSmall url={horror}/>
+      <RowPost title = "Romance" isSmall url = {romance}/>
+      <RowPost title = "Documentries" isSmall url={documentries}/>
     
     </div>
   );
